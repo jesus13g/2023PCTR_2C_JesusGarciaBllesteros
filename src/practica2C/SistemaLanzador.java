b8d9a3770ae97195a2db5b88ce3bec06dc805876
jesus13g <82331712+jesus13g@users.noreply.github.com>
2023-06-06 09:27:24 +0200

    Update SistemaLanzador.java

--- src/practica2C/SistemaLanzador.java
+++ src/practica2C/SistemaLanzador.java
@@ -8,8 +8,8 @@ public class SistemaLanzador {
 		int tipo_enemigos = 4;
 		IJuego juego = new Juego(tipo_enemigos, MAX_enemigos);
 		
-		for(int e = 0; e < tipo_enemigos; e++) {
-			for (int j = 0; j < tipo_enemigos - e; j++) {
+		for(int e = 0; e < tipo_enemigos; e++) { // En cada iteracion, e es un tipo de enemigo 
+			for (int j = 0; j < tipo_enemigos - e; j++) { // De cada tipo se activa varias veces
 				ActividadEnemiga enemigos = new ActividadEnemiga(e, juego);
 				new Thread(enemigos).start();
 				
